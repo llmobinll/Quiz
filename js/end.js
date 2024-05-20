5aecbd3fa03a66f25396d04b5a547de6cf59eac6
llmobinll <mobin30bill@gmail.com>
2024-05-20 22:48:41 +0330

    change a few lines of code

--- js/end.js
+++ js/end.js
@@ -1,30 +1,43 @@
 const score = JSON.parse(localStorage.getItem("score"));
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 const scoreBord = document.getElementById("score-bord");
 const saveButton = document.querySelector(".save");
-const emailInput = document.querySelector("input");
+const input = document.querySelector("input");
 const happyRobot = document.querySelector(".happy");
-const angryRobot = document.querySelector(".angry");
-console.dir(angryRobot);
+const showHand = document.querySelector(".hand");
+const typingText = document.getElementById("typing-text");
+const text = "Your username and score saved successfully";
+console.dir(input);
 scoreBord.innerText = score;
 
-const validateEmail = () => {
-  const char = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const email = emailInput.value;
-  if (email === "") {
-    alert("Pleas enter your Email");
+const validate = (event) => {
+  const userName = input.value;
+
+  if (!userName || !score) {
+    showHand.classList.add("hand2");
+    setTimeout(() => {
+      showHand.style.display = "none";
+    }, 3000);
   } else {
-    if (char.test(email)) {
-      // alert("Your email is Ok!");
-      happyRobot.classList.add("happy2");
-      setTimeout(() => {
-        happyRobot.style.display = "none";
-      }, 5000);
-    } else {
-      // alert("Your email is not correct");
-      angryRobot.classList.add("angry2");
-      setTimeout(() => {
-        angryRobot.style.display = "none";
-      }, 5000);
-    }
+    happyRobot.classList.add("happy2");
+    setTimeout(() => {
+      happyRobot.style.display = "none";
+    }, 5000);
+    const finalScore = { name: userName, score };
+    highScores.push(finalScore);
+    highScores.sort((a, b) => b.score - a.score);
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    localStorage.removeItem("score");
+    highScores.splice(5);
   }
 };
+saveButton.addEventListener("click", (event) => validate(event));
+
+const typeWriter = (text, i, typingText) => {
+  if (i < text.length) {
+    typingText.innerHTML += text.charAt(i);
+    i++;
+    setTimeout(() => typeWriter(text, i, typingText), 100);
+  }
+};
+typeWriter(text, 0, typingText);
